4355a85028ec6eab68dfe3a2b1dd567349588430
Harsha Dharmabandu <harsha.dharmabandu@exactprosystems.com>
2023-06-10 20:25:02 +0530

    dynamic routes

---
+++ Pages/iphone-[name].vue
@@ -0,0 +1,18 @@
+
+<template>
+    <p> This is the page for iphone {{ name }} </p>
+    <p> This is the route {{ route }}</p>
+    <p> This is the route params {{ route.params }}</p>
+    <p> This is route path {{  route.path }}</p>
+</template>
+
+<script setup>
+// save to route
+const route = useRoute() ;
+
+// replace the dashes in the name
+const name = computed(() => {
+    return route.params.name.replaceAll("-", " ") ;
+});
+
+</script>
