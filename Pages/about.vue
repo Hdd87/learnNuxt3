cf1e8db901acf752aee6bdc51fbc70a5433b0213
Harsha Dharmabandu <harsha.dharmabandu@exactprosystems.com>
2023-06-10 17:42:18 +0530

    routing new file:   Pages/about.vue modified:   docs/content/2.routing.md

---
+++ Pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+ <p>
+    this is a sentence from the about.vue component inside pages directory <br />
+
+    but I can still see the counter, and toggle button :(   <br />
+
+    and the development must be restart to see any new changes in dev mode <br />
+
+    http://localhost:3000/about to see this page <br />
+
+    but if change the app.vue file's name to something else and restart dev  <br />
+
+    I can only see the pages inside the pages directory
+</p>
+
+</template>
